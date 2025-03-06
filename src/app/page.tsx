9497cfb22806da7ffe5de93ed765cf354aef9992
Tristan Riboulet <tristanrib_@outlook.fr>
2025-03-07 00:45:39 +0100

    fix(homePage): Target for Gsap initially with 0 opacity to prevent showing up before the gsap animation is called

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,18 +1,12 @@
 "use client"
 
 import styles from './page.module.css';
-import {useEffect} from "react";
-import {gsapNameIn} from "../lib/animation";
 
 export default function Page() {
-    useEffect(() => {
-        gsapNameIn();
-    }, [])
-
     return (
         <main>
             <div className={styles.gsap_nameIn}>
-                <h1 id="nameIn_target">Tristan.Riboulet</h1>
+                <h1 id="nameIn_target" className={'zeroOpacity'}>Tristan.Riboulet</h1>
             </div>
 
             <section className={styles.hero}>
--- src/lib/animation.ts
+++ src/lib/animation.ts
@@ -1,19 +1,22 @@
 import gsap from "gsap";
 import SplitType from "split-type";
 
-export const gsapNameIn = () => {
-    const text = new SplitType('#nameIn_target', {
-        lineClass: 'nameIn-line',
+export const gsapNameIn = (targetId: string = '#nameIn_target') => {
+    const text = new SplitType(targetId, {
+        types: 'words,chars',
         wordClass: 'nameIn-work',
         charClass: 'nameIn-char',
     });
 
     const timeline = gsap.timeline();
 
-    timeline.set('.nameIn-char', {y: 115}).to('.nameIn-char', {
+    timeline.set('.nameIn-char', {y: 128}).to('.nameIn-char', {
         y: 0,
         stagger: 0.05,
         delay: 0.2,
         duration: 0.5,
+        onStart: () => {
+            document.querySelector(targetId)?.classList.remove("zeroOpacity");
+        }
     });
 }
