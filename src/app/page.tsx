47d934881f800ca4f176ac915606f606fb03d4cb
TristanRib <tristanrib_@outlook.fr>
2025-03-07 19:15:19 +0100

    feat(masks): test implementation

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,15 +1,31 @@
 "use client"
 
-import styles from './page.module.css';
+import styles from './page.module.scss';
+import {useEffect} from "react";
+import {gsapNameIn} from "@/lib/animation";
 
 export default function Page() {
+    useEffect(() => {
+        gsapNameIn();
+    }, [])
+
     return (
         <main>
-            <div className={styles.gsap_nameIn}>
-                <h1 id="nameIn_target" className={'zeroOpacity'}>Tristan.Riboulet</h1>
-            </div>
+            {/*<div className={styles.gsap_nameIn}>*/}
+            {/*    <h1 id="nameIn-target">Tristan.Riboulet</h1>*/}
+            {/*</div>*/}
 
             <section className={styles.hero}>
+                <div className={"frame"}>
+                    <div className={"frame_mask"}>
+                        <div className={"frame_outer"}>
+
+                        </div>
+                        <div className={"frame_content"}>
+                            <img src={"/thumb-1920-1359851.jpeg"} alt={""}/>
+                        </div>
+                    </div>
+                </div>
             </section>
         </main>
     );
--- src/lib/animation.ts
+++ src/lib/animation.ts
@@ -1,22 +1,23 @@
+"use client"
+
 import gsap from "gsap";
 import SplitType from "split-type";
 
-export const gsapNameIn = (targetId: string = '#nameIn_target') => {
+export const gsapNameIn = (targetId: string = '#nameIn-target') => {
     const text = new SplitType(targetId, {
         types: 'words,chars',
-        wordClass: 'nameIn-work',
+        wordClass: 'nameIn-word',
         charClass: 'nameIn-char',
     });
 
     const timeline = gsap.timeline();
 
-    timeline.set('.nameIn-char', {y: 128}).to('.nameIn-char', {
+    timeline.set('.nameIn-char', {y: 128});
+    timeline.set(targetId, {opacity: 1});
+    timeline.to('.nameIn-char', {
         y: 0,
         stagger: 0.05,
         delay: 0.2,
         duration: 0.5,
-        onStart: () => {
-            document.querySelector(targetId)?.classList.remove("zeroOpacity");
-        }
     });
 }
